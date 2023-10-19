08e301de1cd4adb07692cb35000fcc54bd0b3a02
phamvannhatvu <vu.pham2512@hcmut.edu.vn>
2023-10-19 14:27:45 +0700

    Initial commit

---
+++ Button/Core/Inc/button.h
@@ -0,0 +1,21 @@
+/*
+ * button.h
+ *
+ *  Created on: Oct 5, 2023
+ *      Author: KAI
+ */
+
+#ifndef INC_BUTTON_H_
+#define INC_BUTTON_H_
+
+#include "main.h"
+
+#define NORMAL_STATE SET
+#define PRESSED_STATE RESET
+
+int isButton1Pressed();
+int isButton1LongPressed();
+void getKeyInput();
+
+#endif /* INC_BUTTON_H_ */
+
